085869784d7fcd14a7ba69e4287280f8e5619450
Kendrick McWha <kendrick.mcwha@outlook.com>
2024-09-03 13:41:54 -0400

    2024-09-03

---
+++ MoneyControl.Domain/Migrations/20240903165326_20240903.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoneyControl.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20240903 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegularPaymentId",
+                table: "TRANSACTIONS");
+
+            migrationBuilder.RenameColumn(
+                name: "Transactionid",
+                table: "SUBTRANSACTION",
+                newName: "TransactionId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransDate",
+                table: "TRANSACTIONS",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PostDate",
+                table: "TRANSACTIONS",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BudgetDate",
+                table: "TRANSACTIONS",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "SUBTRANSACTION",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PayeeId",
+                table: "SUBTRANSACTION",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RegularPaymentId",
+                table: "SUBTRANSACTION",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "SUBTRANSACTION");
+
+            migrationBuilder.DropColumn(
+                name: "PayeeId",
+                table: "SUBTRANSACTION");
+
+            migrationBuilder.DropColumn(
+                name: "RegularPaymentId",
+                table: "SUBTRANSACTION");
+
+            migrationBuilder.RenameColumn(
+                name: "TransactionId",
+                table: "SUBTRANSACTION",
+                newName: "Transactionid");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "TransDate",
+                table: "TRANSACTIONS",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "PostDate",
+                table: "TRANSACTIONS",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "BudgetDate",
+                table: "TRANSACTIONS",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RegularPaymentId",
+                table: "TRANSACTIONS",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
